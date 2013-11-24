743c41da82cad1cd5291342b62367302a643387b
unknown <C:\Users\\AppData\Roaming\The Bat!>
2013-11-24 21:07:35 +0400

    BD, UserModel

---
+++ MySite/Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MySite.Models
+{
+    public class UserModel
+    {
+        private string connStr = "";
+
+        public string[] GetUserNames()
+        {
+            SqlConnection myConn = new SqlConnection(connStr);
+            myConn.Open();
+
+            SqlCommand com = myConn.CreateCommand();
+            com.CommandText = "SELECT User FROM User";
+
+            SqlDataReader reader = com.ExecuteReader();
+
+            List<string> Users = new List<string>();
+
+            while(reader.Read())
+            {
+                Users.Add(reader["User"].ToString());
+            }
+
+            return Users.ToArray();
+        }
+    }
+}
